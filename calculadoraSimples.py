1ba50a99164ed1d0c502569d22652cb4fc98f335
Rogersamacedo <41793312+Rogersamacedo@users.noreply.github.com>
2021-09-13 12:27:33 -0300

    Add files via upload

---
+++ calculadoraSimples.py
@@ -0,0 +1,24 @@
+#aula7 - exercicio 5
+n = int(input('Didite um numero; '))
+a = n - 1
+p = n + 1
+print( n )
+print('antecessor;', a)
+print(' posterior:', p)
+
+
+#exercicio 6
+n = int(input( 'digite um numero: '))
+print( "Dobro:", n*2)
+print("triplo: ", n*3)
+print("Raiz: ", n**0.5)
+
+#exercicio 7
+n = float(input( 'nota de matematica: '))
+n2 = float(input('nota de portugues; '))
+m = ( n + n2) / 2
+print( n )
+print( n2)
+print( m)
+       
+
